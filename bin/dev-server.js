3b2caf6e45b0026f3431b78edd0eb006ad117562
Kirill Lahtachev <lahtachev@gmail.com>
2018-07-30 19:32:15 +0400

    add Flow

--- bin/dev-server.js
+++ bin/dev-server.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 const serve = require(`webpack-serve`);
 const config = require(`../webpack.config`);
+const argv = {};
 
-serve({ config, clipboard: false });
+serve(argv, { config, clipboard: false });
