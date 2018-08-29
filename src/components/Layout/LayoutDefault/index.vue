1b1a36a02d03e2eacd652813a916de6ec8968897
Kirill Lahtachev <lahtachev@gmail.com>
2018-08-29 17:49:45 +0400

    add new burger button

--- src/components/Layout/LayoutDefault/index.vue
+++ src/components/Layout/LayoutDefault/index.vue
@@ -3,6 +3,12 @@
 
     <button class="menu-toggle" @click="menuToggle"></button>
     <nav-menu/>
+    
+    <div class="container">
+      <div class="menu" @click="hambergerOpen = !hambergerOpen">
+        <div class="hambergerIcon" :class="{open: hambergerOpen}" ></div>
+      </div>
+    </div>
 
     <slot name="header">
       <!-- <app-header/> -->
@@ -28,6 +34,7 @@ export default {
   data() {
     return {
       open: false,
+      hambergerOpen: false,
     };
   },
 
@@ -146,5 +153,118 @@ button {
       transition: transform 0.4s 1s ease-out, opacity 0.4s 1s ease-out;
     }
   }
+}
+
+$t: 0.2s;
+$tf: 0.3s;
+$delay: 0.2s;
+
+.container {
+  position: fixed;
+  z-index: 2;
+  top: 18px;
+  right: 28px;
+  margin: auto;
+}
+.menu {
+  height: 40px;
+  width: 40px;
+  position: relative;
+  // top: 50%;
+  // left: 50%;
+  // transform: translate(-50%, -50%);
+  &:hover {
+    cursor: pointer;
+  }
+}
+
+.hambergerIcon {
+  height: 5px;
+  width: 35px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  border-radius: 50px;
+  background-color: #4a2545;
+  // transform: translate(-50%,-50%) rotate(0deg);
+  animation: rotateBack $t ease-out forwards;
+  // animation-direction: reverse;
+  // animation-direction: reverse; // transform: translate(-50%,-50%) rotate(0deg);
+  // transition: all ease 0.5s;
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    height: inherit;
+    border-radius: inherit;
+    background-color: inherit;
+    margin: auto;
+    width: 100%;
+    transition: all ease 0.5s;
+  }
+  &::before {
+    top: -10px;
+    left: 0;
+    transform-origin: center;
+    // animation: rotate $t $delay ease-out forwards;
+  }
+  &::after {
+    bottom: -10px;
+    left: 0;
+    transform-origin: center;
+    animation: slideToCenterY $t ease-out forwards;
+  }
+}
+.open {
+  // transform: translate(-50%,-50%) rotate(135deg);
+  // animation: rotate $t ease-out;
+  animation: rotate $t ease-out forwards;
+  // animation-direction: reverse;
+  &::before {
+    top: 0;
+    animation: slideToCenterY $t ease-out forwards,
+      rotate3 $tf $delay ease-out forwards;
+    // transform: translate(0, 0%) rotate(-90deg);
+  }
+  &::after {
+    bottom: 0;
+    transform: translate(0, 0%) rotate(-90deg);
+  }
+}
+@keyframes rotateBack {
+  from {
+    transform: translate(-50%, -50%) rotate(135deg);
+  }
+  to {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+}
+@keyframes rotate45 {
+  to {
+    transform: translate(-50%, -50%) rotate(135deg);
+  }
+}
+@keyframes rotate3 {
+  to {
+    // transform: translate(0, 0%) rotate(450deg);
+    transform: translate(0, 0%) rotate(90deg);
+  }
+}
+@keyframes rotate {
+  from {
+    transform: translate(-50%, -50%) rotate(0deg);
+  }
+  to {
+    transform: translate(-50%, -50%) rotate(135deg);
+  }
+}
+@keyframes slideToCenterY {
+  to {
+    transform: translateY(0);
+  }
+  // 60% {
+  // }
+  // 100% {
+  // }
 }
 </style>
